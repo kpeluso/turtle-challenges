458a5759bd1993efa5bfdd974ee33d6f12408f52
Kenny Peluso <pelusoken@gmail.com>
2017-01-06 16:27:13 -0500

    peter right there baby ohhhh yahhh pt.2 right in the kisser initial files right there baby

---
+++ turt.py
@@ -0,0 +1,12 @@
+import turtle
+
+turtle.home() # starts program and puts turtle in the center of the window
+
+# Put Movement/Special commands here:
+
+
+
+
+
+turtle.exitonclick() # the window will close when you click on it
+
